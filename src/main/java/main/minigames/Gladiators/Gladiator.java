ddc84ebcea932ba47fb88e434849837d51157828
AshtonA1021 <aamouri1021@icloud.com>
2023-03-24 13:18:00 -0400

    Added in base items and clears inventory on game starting

--- src/main/java/main/minigames/Gladiators/Gladiator.java
+++ src/main/java/main/minigames/Gladiators/Gladiator.java
@@ -4,6 +4,7 @@ import main.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -73,14 +74,36 @@ public class Gladiator implements Listener {
 			Bukkit.broadcastMessage("Game starting, kill the other opponent ");
 			List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
 			int i = 1;
-			ItemStack item = new ItemStack(Material.STICK);
+			ItemStack diassword = new ItemStack(Material.DIAMOND_SWORD);
+			diassword.addEnchantment(Enchantment.DAMAGE_ALL, 2);
+			ItemStack diamondhelm = new ItemStack(Material.DIAMOND_HELMET);
+			diamondhelm.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2);
+			diamondhelm.addEnchantment(Enchantment.DURABILITY, 1);
+
+			ItemStack diamondchest = new ItemStack(Material.DIAMOND_CHESTPLATE);
+			diamondchest.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2);
+			diamondchest.addEnchantment(Enchantment.DURABILITY, 1);
+
+			ItemStack diamondlegs = new ItemStack(Material.DIAMOND_LEGGINGS);
+			diamondlegs.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2);
+			diamondlegs.addEnchantment(Enchantment.DURABILITY, 1);
+
+			ItemStack diamondboots = new ItemStack(Material.DIAMOND_BOOTS);
+			diamondboots.addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2);
+			diamondboots.addEnchantment(Enchantment.DURABILITY, 1);
 			for (Player player : players) {
 				player.teleport(i == 1 ? spot1 : spot2);
 				i++;
 			}
 			for (Player player : players) {
 				player.sendMessage("HEY game is starting ");
-				player.getInventory().addItem(item);
+				player.getInventory().clear();
+				player.getInventory().setHelmet(diamondhelm);
+				player.getInventory().setChestplate(diamondchest);
+				player.getInventory().setLeggings(diamondlegs);
+				player.getInventory().setBoots(diamondboots);
+
+				player.getInventory().addItem(diassword);
 			}
 		}, COOLDOWN_TIME_SECONDS * 20L);
 	}
